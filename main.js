336e0e15dc242fac925517676d242072c7aef474
alenabellmays <Uhlenabelly>
2023-03-16 20:28:29 -0700

    changed js and html file

--- main.js
+++ main.js
@@ -1,4 +1,52 @@
-import { data } from './getData.js'
+import { data } from "./getData.js";
 
+const container = document.querySelector(".container");
+const searchBtn = document.getElementsByTagName("button");
+const input = document.querySelector("input");
 const drinksData = data();
-console.log(drinksData, '<<<<this is my imported data')
+
+function showDrinksOnUI(arr) {
+  for (let drink of arr) {
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    const drinkName = document.createElement("h3");
+    drinkName.innerHTML = drink.strDrink;
+    img.src = drink.strDrinkThumb;
+    div.appendChild(drinkName);
+    div.appendChild(img);
+    container.appendChild(div);
+  }
+}
+
+function searchFilter(inputVal) {
+  const filteredDrinks = [];
+  for (let el of drinksData) {
+    let nameOfDrinks = el.strDrink.toLowerCase();
+    if (nameOfDrinks.includes(inputVal.toLowerCase())) {
+      filteredDrinks.push(el);
+    }
+  }
+  return filteredDrinks;
+}
+function clear() {
+  while (container.firstchild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+function clickEvent() {
+  if (input.value === "") {
+    //console.log('input value is empty' )
+    clear();
+    showDrinksOnUI(drinksData);
+  } else {
+    const inputValue = input.value;
+    const drinks = searchFilter(inputValue);
+    console.log(drinks, "<<<drinks");
+    clear();
+    showDrinksOnUI(drinks);
+  }
+}
+input.addEventListener("keyup", clickEvent);
+
+showDrinksOnUI(drinksData);
